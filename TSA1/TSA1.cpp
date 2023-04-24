425301a882c46a3408edf6c23f7320e7e12b6642
Carl Justine F. Malabanan <carljustinem984@gmail.com>
2023-04-25 00:08:22 +0800

    MADE BY AI, WILL REDO IT LATER

--- TSA1/TSA1.cpp
+++ TSA1/TSA1.cpp
@@ -4,7 +4,39 @@
 using namespace std;
 int main()
 {
-   
+    Array1D arr(5);
+
+    arr.AddItem(10);
+    arr.AddItem(20);
+    arr.AddItem(30);
+
+    arr.DisplayElements();
+
+    arr.InsertItem(1, 15);
+
+    arr.DisplayElements();
+
+    arr.AddItem(40);
+
+    arr.DisplayElements();
+
+    arr.AddItem(50);
+
+    arr.RemoveItem(0);
+
+    arr.DisplayElements();
+
+    cout << "Array size: " << arr.GetSize() << endl;
+    if (arr.isEmpty())
+    {
+        cout << "Array is empty!" << endl;
+    }
+    else
+    {
+        cout << "Array is not empty!" << endl;
+    }
+
+
 }
 
 
